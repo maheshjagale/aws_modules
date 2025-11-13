981f7036d3c5ae54d7a02322075005f15dfb12ba
Mahesh Jagale <maheshjagale93@gmail.com>
2025-11-13 11:57:19 +0530

    Create main.tf

---
+++ modules/iam/main.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_role" "main" {
+  name = "my-iam-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "main" {
+  role       = aws_iam_role.main.name
+  policy_arn = var.iam_policy_arn
+}
