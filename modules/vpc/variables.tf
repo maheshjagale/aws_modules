a77380aa4890efd82037a17bc76ad6c893aec9d6
Mahesh Jagale <maheshjagale93@gmail.com>
2025-11-13 11:51:19 +0530

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_cidr_block" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
