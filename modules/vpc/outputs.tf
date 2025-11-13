345c4431d5b8925d7a8dfa372676e8a8abdb9e2a
Mahesh Jagale <maheshjagale93@gmail.com>
2025-11-13 11:52:12 +0530

    Create outputs.tf

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "default_security_group_id" {
+  description = "ID of the default security group"
+  value       = aws_vpc.main.default_security_group_id
+}
