a71616f9e2e9fca033ac3601c2d78fa235903691
Mahesh Jagale <maheshjagale93@gmail.com>
2025-11-13 12:01:03 +0530

    Create variables.tf

---
+++ modules/load_balancer/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for ALB"
+  type        = list(string)
+}
