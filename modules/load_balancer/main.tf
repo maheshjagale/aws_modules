fe0463f957c855c6ccb93cc67bdd4612aae50699
Mahesh Jagale <maheshjagale93@gmail.com>
2025-11-13 12:00:43 +0530

    Create main.tf

---
+++ modules/load_balancer/main.tf
@@ -0,0 +1,25 @@
+resource "aws_lb" "main" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = var.subnet_ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_security_group" "alb" {
+  name        = "alb-sg"
+  description = "Allow HTTP traffic"
+  vpc_id      = var.vpc_id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
