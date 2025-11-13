2a4877149f2f01a23f6e0ab673dee3661c1f9044
Mahesh Jagale <maheshjagale93@gmail.com>
2025-11-13 11:50:36 +0530

    Create main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,30 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  tags = {
+    Name = "private-subnet-${count.index}"
+  }
+}
+
+data "aws_availability_zones" "available" {}
